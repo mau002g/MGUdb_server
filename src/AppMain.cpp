8f07fa70596702dd37bdabd13eec80ca1057b9c4
mau002g <113718250+mau002g@users.noreply.github.com>
2022-11-25 23:00:03 -0400

    Add files via upload

---
+++ src/AppMain.cpp
@@ -0,0 +1,21 @@
+#include "AppMain.hpp"
+
+namespace mgu
+{
+    //Constructor
+    AppMain::AppMain()
+    {
+        std::cout << "Iniciando..." << std::endl;
+    }
+    //Destructor
+    AppMain::~AppMain()
+    {
+        std::cout << "Saliendo..." << std::endl;
+    }
+    //Función principal
+    int AppMain::Run()
+    {
+        std::cout << "Hola mundo" << std::endl;
+        return 0;
+    }
+}
---
+++ src/AppMain.hpp
@@ -0,0 +1,24 @@
+#ifndef APP_MAIN_HPP
+#define APP_MAIN_HPP
+
+#include <iostream>
+
+namespace mgu
+{
+    class AppMain
+    {
+        public:
+
+        //Constructor
+        AppMain();
+        //Destructor
+        ~AppMain();
+
+        //Funciones
+        int Run();
+
+        private:
+    }; 
+}
+
+#endif //APP_MAIN_HPP
